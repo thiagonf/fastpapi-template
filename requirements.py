63ba4971d42cb78ed3755a0e857e808630884500
Thiago Nogueira <thiagonf10@gmail.com>
2020-10-25 11:59:03 -0300

    Adding gitignore and requirements

---
+++ requirements.py
@@ -0,0 +1,9 @@
+fastapi == 0.61.1
+uvicorn == 0.12.2
+python-multipart == 0.0.5
+python-jose[cryptography] == 3.2.0
+passlib[bcrypt]
+sqlalchemy == 1.3.20
+pydantic[email] == 1.6.1
+async-exit-stack == 1.0.1
+async-generator == 1.10
